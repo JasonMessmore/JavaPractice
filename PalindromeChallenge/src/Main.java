f6da17d5983e4c18ccb6957d5276ef44a605ead8
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-05-09 22:24:29 -0600

    added firstAndLastDigitSum Challenge from Udemy to the bottom

--- PalindromeChallenge/src/Main.java
+++ PalindromeChallenge/src/Main.java
@@ -18,3 +18,19 @@ public static void main(int)
         }
     return b; }
 }
+
+// First and Last Digit Sum Challenge below
+// public class FirstLastDigitSum {
+//    public static int sumFirstAndLastDigit(int number) {
+//        if (number < 0) {
+//            return -1;
+//        }
+//        int firstDigit = 0;
+//        int lastDigit = number % 10;
+//        while ((number / 1) >= 10) {
+//            number /= 10;
+//        }
+//        firstDigit = number;
+//        return (firstDigit + lastDigit);
+//    }
+//}
