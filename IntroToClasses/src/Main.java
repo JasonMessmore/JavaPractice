5135f2d25923065be95cd4e7c2820dedb7993c07
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-08-01 15:15:28 -0600

    Added validation for the car make attribute, as well as a second instance for the car object

--- IntroToClasses/src/Main.java
+++ IntroToClasses/src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car civic = new Car();
+
+        civic.setConvertible(false);
+        civic.setDoors(4);
+        civic.setMake("Honda");
+        civic.setModel("Civic");
+        civic.setColor("Yellow");
+
         Car car = new Car();
+
+        car.setMake("Porsche");
+        car.setModel("911");
+        car.setColor("Orange");
+        car.setDoors(2);
+        car.setConvertible(true);
 
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+        civic.describeCar();
     }
 }
