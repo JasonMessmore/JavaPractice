0c71166e3479ade3b2c776a525496d8cee8d1a47
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-07-31 22:05:26 -0600

    First taste of OOP finally

---
+++ IntroToClasses/src/Car.java
@@ -0,0 +1,37 @@
+public class Car {
+
+    private String make = "Honda";
+    private String model = "Accord";
+    private String color = "White";
+    private int doors = 4;
+    private boolean convertible = false;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void describeCar() {
+
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
