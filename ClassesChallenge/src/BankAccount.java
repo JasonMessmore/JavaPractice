e2937519ef3fd53bd76d24c8a99d07551d8b726f
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-08-01 16:28:28 -0600

    Completed Classes Challenge from Udemy

---
+++ ClassesChallenge/src/BankAccount.java
@@ -0,0 +1,82 @@
+public class BankAccount {
+
+    private double accountNumber = 0;
+    private double accountBalance = 0;
+    private String customerName = "John";
+    private String customerEmail = "john@example.com";
+    private long customerPhone = 614234567;
+
+    public double getAccountNumber() {
+        return accountNumber;
+    }
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    public long getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setAccountNumber(double accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setCustomerPhone(long customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    private double withdrawalAmount = 0;
+
+    public double getWithdrawalAmount() {
+        return withdrawalAmount;
+    }
+
+    public void setWithdrawalAmount(double withdrawalAmount) {
+        this.withdrawalAmount = withdrawalAmount;
+    }
+    int status = 0;
+    public void withdrawalFunds() {
+        if (withdrawalAmount > accountBalance) {
+            System.out.println("Insufficient Funds Available");
+            System.exit(1);
+        }
+        accountBalance = accountBalance - withdrawalAmount;
+    }
+
+    public void depositFunds() {
+        accountBalance = accountBalance + depositAmount;
+    }
+
+    private double depositAmount = 0;
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public String newLine = System.getProperty("line.separator");
+    public void accountInfo() {
+        System.out.println(
+                "Name: " + customerName + newLine +
+                "Balance: " + accountBalance);
+    }
+}
---
+++ ClassesChallenge/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount first = new BankAccount();
+
+        first.setAccountNumber(1234567899);
+        first.setAccountBalance(200000);
+        first.setCustomerName("John Brooks");
+        first.setCustomerEmail("johnbrooks@example.com");
+        first.setCustomerPhone(614123495);
+        first.setWithdrawalAmount(200000);
+
+        first.setWithdrawalAmount(200001);
+        first.withdrawalFunds();
+
+
+        first.accountInfo();
+    }
+}
