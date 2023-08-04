9b8eca9090ec2301bd95e08343dee36733354b25
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-08-03 21:21:33 -0600

    Completed the Simple Calculator challenge on Udemy

---
+++ ClassesChallenge/src/SimpleCalculator.java
@@ -0,0 +1,46 @@
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        if (secondNumber == 0) {
+            return firstNumber;
+        }
+        else if (firstNumber == 0) {
+            if (secondNumber > 0) {
+                return secondNumber * -1;
+            }
+            return secondNumber;
+        }
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) return 0;
+        return firstNumber / secondNumber;
+    }
+}
