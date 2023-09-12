4a7107c4def0b8762a957ffb77fbd05bf9969f1f
JasonMessmore <107559071+JasonMessmore@users.noreply.github.com>
2023-09-12 17:39:53 -0600

    added hourlyEmployee to main

--- OOP/InheritanceChallenge/src/Main.java
+++ OOP/InheritanceChallenge/src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         System.out.println("Pay = " + jason.collectPay());
         int jasonAge = jason.getAge();
 
+        HourlyEmployee sam = new HourlyEmployee("Sam", "12/02/1998", "07/10/2021", 34.73);
+        System.out.println(sam);
+        System.out.println("Sam's age is: " + sam.getAg e());
+        System.out.println("Sam's paycheck = " + sam.collectPay());
+
         SalariedEmployee joe = new SalariedEmployee("Joe", "10/12/1998",
                 "03/20/2020", 250000);
         System.out.println(joe);
@@ -14,6 +19,8 @@ public class Main {
 
         joe.retire();
         System.out.println("Joe's retirement check will be $" + joe.collectPay());
+
+
 
     }
 }
